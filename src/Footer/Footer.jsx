e37172c531cf5e4814a05805efc0914a32c9e352
= <=>
2023-04-20 00:05:38 +0500

    Updated footer info

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import './Footer.css'
 
 export default function Footer () {
   return (
-    <footer id='footer' className='flex-center'><span>Developed by <a href='http://linkedin.com/ziadevcom'>Zia</a></span>
+    <footer id='footer' className='flex-center'><p>This is a clone of <a href='https://ztype.ziadev.com'>zType</a> with React | <a href='https://github.com/ziadevcom'>My Github</a></p>
     </footer>
   )
 }

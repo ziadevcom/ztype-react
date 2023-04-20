b778f049a03ed20fbe3a31e955ff7a454d86646e
= <=>
2023-04-21 03:08:02 +0500

    hid the input element where user will type

--- src/Typer/userText/userText.jsx
+++ src/Typer/userText/userText.jsx
@@ -6,7 +6,7 @@ export default function userText ({ userText, onUserTextChange, testOnGoing }) {
   if (testOnGoing) inputRef.current.focus()
 
   return (
-    <div id='userTypedText' className=''>
+    <div id='userTypedText' className='sr-only'>
       <label htmlFor='userTypedText__input'>Start typing the text you see on the screen</label>
       <input ref={inputRef} type='text' id='userTypedText__input' value={userText} onChange={onUserTextChange} />
     </div>
